a2193cc6e7fe3af6576696ab90cdabcc991a3ae1
Diego Vogel <diego@birdboar.co>
2023-07-12 22:10:15 -0400

    Misc setup Replaced theme namespace. Added normalize. Set up styling file structure.

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
-add_action( 'after_setup_theme', 'blankslate_setup' );
-function blankslate_setup() {
-load_theme_textdomain( 'blankslate', get_template_directory() . '/languages' );
+add_action( 'after_setup_theme', 'diegoworks_setup' );
+function diegoworks_setup() {
+load_theme_textdomain( 'diegoworks', get_template_directory() . '/languages' );
 add_theme_support( 'title-tag' );
 add_theme_support( 'post-thumbnails' );
 add_theme_support( 'responsive-embeds' );
@@ -10,29 +10,29 @@ add_theme_support( 'html5', array( 'search-form', 'navigation-widgets' ) );
 add_theme_support( 'woocommerce' );
 global $content_width;
 if ( !isset( $content_width ) ) { $content_width = 1920; }
-register_nav_menus( array( 'main-menu' => esc_html__( 'Main Menu', 'blankslate' ) ) );
+register_nav_menus( array( 'main-menu' => esc_html__( 'Main Menu', 'diegoworks' ) ) );
 }
-add_action( 'admin_notices', 'blankslate_notice' );
-function blankslate_notice() {
+add_action( 'admin_notices', 'diegoworks_notice' );
+function diegoworks_notice() {
 $user_id = get_current_user_id();
 $admin_url = ( isset( $_SERVER['HTTPS'] ) && $_SERVER['HTTPS'] === 'on' ? 'https' : 'http' ) . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 $param = ( count( $_GET ) ) ? '&' : '?';
-if ( !get_user_meta( $user_id, 'blankslate_notice_dismissed_8' ) && current_user_can( 'manage_options' ) )
-echo '<div class="notice notice-info"><p><a href="' . esc_url( $admin_url ), esc_html( $param ) . 'dismiss" class="alignright" style="text-decoration:none"><big>' . esc_html__( 'Ⓧ', 'blankslate' ) . '</big></a>' . wp_kses_post( __( '<big><strong>📝 Thank you for using BlankSlate!</strong></big>', 'blankslate' ) ) . '<br /><br /><a href="https://wordpress.org/support/theme/blankslate/reviews/#new-post" class="button-primary" target="_blank">' . esc_html__( 'Review', 'blankslate' ) . '</a> <a href="https://github.com/tidythemes/blankslate/issues" class="button-primary" target="_blank">' . esc_html__( 'Feature Requests & Support', 'blankslate' ) . '</a> <a href="https://calmestghost.com/donate" class="button-primary" target="_blank">' . esc_html__( 'Donate', 'blankslate' ) . '</a></p></div>';
+if ( !get_user_meta( $user_id, 'diegoworks_notice_dismissed_8' ) && current_user_can( 'manage_options' ) )
+echo '<div class="notice notice-info"><p><a href="' . esc_url( $admin_url ), esc_html( $param ) . 'dismiss" class="alignright" style="text-decoration:none"><big>' . esc_html__( 'Ⓧ', 'diegoworks' ) . '</big></a>' . wp_kses_post( __( '<big><strong>📝 Thank you for using BlankSlate!</strong></big>', 'diegoworks' ) ) . '<br /><br /><a href="https://wordpress.org/support/theme/blankslate/reviews/#new-post" class="button-primary" target="_blank">' . esc_html__( 'Review', 'diegoworks' ) . '</a> <a href="https://github.com/tidythemes/blankslate/issues" class="button-primary" target="_blank">' . esc_html__( 'Feature Requests & Support', 'diegoworks' ) . '</a> <a href="https://calmestghost.com/donate" class="button-primary" target="_blank">' . esc_html__( 'Donate', 'diegoworks' ) . '</a></p></div>';
 }
-add_action( 'admin_init', 'blankslate_notice_dismissed' );
-function blankslate_notice_dismissed() {
+add_action( 'admin_init', 'diegoworks_notice_dismissed' );
+function diegoworks_notice_dismissed() {
 $user_id = get_current_user_id();
 if ( isset( $_GET['dismiss'] ) )
-add_user_meta( $user_id, 'blankslate_notice_dismissed_8', 'true', true );
+add_user_meta( $user_id, 'diegoworks_notice_dismissed_8', 'true', true );
 }
-add_action( 'wp_enqueue_scripts', 'blankslate_enqueue' );
-function blankslate_enqueue() {
-wp_enqueue_style( 'blankslate-style', get_stylesheet_uri() );
+add_action( 'wp_enqueue_scripts', 'diegoworks_enqueue' );
+function diegoworks_enqueue() {
+wp_enqueue_style( 'diegoworks-style', get_stylesheet_uri() );
 wp_enqueue_script( 'jquery' );
 }
-add_action( 'wp_footer', 'blankslate_footer' );
-function blankslate_footer() {
+add_action( 'wp_footer', 'diegoworks_footer' );
+function diegoworks_footer() {
 ?>
 <script>
 jQuery(document).ready(function($) {
@@ -64,20 +64,20 @@ $("html").addClass("opera");
 </script>
 <?php
 }
-add_filter( 'document_title_separator', 'blankslate_document_title_separator' );
-function blankslate_document_title_separator( $sep ) {
+add_filter( 'document_title_separator', 'diegoworks_document_title_separator' );
+function diegoworks_document_title_separator( $sep ) {
 $sep = esc_html( '|' );
 return $sep;
 }
-add_filter( 'the_title', 'blankslate_title' );
-function blankslate_title( $title ) {
+add_filter( 'the_title', 'diegoworks_title' );
+function diegoworks_title( $title ) {
 if ( $title == '' ) {
 return esc_html( '...' );
 } else {
 return wp_kses_post( $title );
 }
 }
-function blankslate_schema_type() {
+function diegoworks_schema_type() {
 $schema = 'https://schema.org/';
 if ( is_single() ) {
 $type = "Article";
@@ -90,45 +90,45 @@ $type = 'WebPage';
 }
 echo 'itemscope itemtype="' . esc_url( $schema ) . esc_attr( $type ) . '"';
 }
-add_filter( 'nav_menu_link_attributes', 'blankslate_schema_url', 10 );
-function blankslate_schema_url( $atts ) {
+add_filter( 'nav_menu_link_attributes', 'diegoworks_schema_url', 10 );
+function diegoworks_schema_url( $atts ) {
 $atts['itemprop'] = 'url';
 return $atts;
 }
-if ( !function_exists( 'blankslate_wp_body_open' ) ) {
-function blankslate_wp_body_open() {
+if ( !function_exists( 'diegoworks_wp_body_open' ) ) {
+function diegoworks_wp_body_open() {
 do_action( 'wp_body_open' );
 }
 }
-add_action( 'wp_body_open', 'blankslate_skip_link', 5 );
-function blankslate_skip_link() {
-echo '<a href="#content" class="skip-link screen-reader-text">' . esc_html__( 'Skip to the content', 'blankslate' ) . '</a>';
+add_action( 'wp_body_open', 'diegoworks_skip_link', 5 );
+function diegoworks_skip_link() {
+echo '<a href="#content" class="skip-link screen-reader-text">' . esc_html__( 'Skip to the content', 'diegoworks' ) . '</a>';
 }
-add_filter( 'the_content_more_link', 'blankslate_read_more_link' );
-function blankslate_read_more_link() {
+add_filter( 'the_content_more_link', 'diegoworks_read_more_link' );
+function diegoworks_read_more_link() {
 if ( !is_admin() ) {
-return ' <a href="' . esc_url( get_permalink() ) . '" class="more-link">' . sprintf( __( '...%s', 'blankslate' ), '<span class="screen-reader-text">  ' . esc_html( get_the_title() ) . '</span>' ) . '</a>';
+return ' <a href="' . esc_url( get_permalink() ) . '" class="more-link">' . sprintf( __( '...%s', 'diegoworks' ), '<span class="screen-reader-text">  ' . esc_html( get_the_title() ) . '</span>' ) . '</a>';
 }
 }
-add_filter( 'excerpt_more', 'blankslate_excerpt_read_more_link' );
-function blankslate_excerpt_read_more_link( $more ) {
+add_filter( 'excerpt_more', 'diegoworks_excerpt_read_more_link' );
+function diegoworks_excerpt_read_more_link( $more ) {
 if ( !is_admin() ) {
 global $post;
-return ' <a href="' . esc_url( get_permalink( $post->ID ) ) . '" class="more-link">' . sprintf( __( '...%s', 'blankslate' ), '<span class="screen-reader-text">  ' . esc_html( get_the_title() ) . '</span>' ) . '</a>';
+return ' <a href="' . esc_url( get_permalink( $post->ID ) ) . '" class="more-link">' . sprintf( __( '...%s', 'diegoworks' ), '<span class="screen-reader-text">  ' . esc_html( get_the_title() ) . '</span>' ) . '</a>';
 }
 }
 add_filter( 'big_image_size_threshold', '__return_false' );
-add_filter( 'intermediate_image_sizes_advanced', 'blankslate_image_insert_override' );
-function blankslate_image_insert_override( $sizes ) {
+add_filter( 'intermediate_image_sizes_advanced', 'diegoworks_image_insert_override' );
+function diegoworks_image_insert_override( $sizes ) {
 unset( $sizes['medium_large'] );
 unset( $sizes['1536x1536'] );
 unset( $sizes['2048x2048'] );
 return $sizes;
 }
-add_action( 'widgets_init', 'blankslate_widgets_init' );
-function blankslate_widgets_init() {
+add_action( 'widgets_init', 'diegoworks_widgets_init' );
+function diegoworks_widgets_init() {
 register_sidebar( array(
-'name' => esc_html__( 'Sidebar Widget Area', 'blankslate' ),
+'name' => esc_html__( 'Sidebar Widget Area', 'diegoworks' ),
 'id' => 'primary-widget-area',
 'before_widget' => '<li id="%1$s" class="widget-container %2$s">',
 'after_widget' => '</li>',
@@ -136,25 +136,25 @@ register_sidebar( array(
 'after_title' => '</h3>',
 ) );
 }
-add_action( 'wp_head', 'blankslate_pingback_header' );
-function blankslate_pingback_header() {
+add_action( 'wp_head', 'diegoworks_pingback_header' );
+function diegoworks_pingback_header() {
 if ( is_singular() && pings_open() ) {
 printf( '<link rel="pingback" href="%s" />' . "\n", esc_url( get_bloginfo( 'pingback_url' ) ) );
 }
 }
-add_action( 'comment_form_before', 'blankslate_enqueue_comment_reply_script' );
-function blankslate_enqueue_comment_reply_script() {
+add_action( 'comment_form_before', 'diegoworks_enqueue_comment_reply_script' );
+function diegoworks_enqueue_comment_reply_script() {
 if ( get_option( 'thread_comments' ) ) {
 wp_enqueue_script( 'comment-reply' );
 }
 }
-function blankslate_custom_pings( $comment ) {
+function diegoworks_custom_pings( $comment ) {
 ?>
 <li <?php comment_class(); ?> id="li-comment-<?php comment_ID(); ?>"><?php echo esc_url( comment_author_link() ); ?></li>
 <?php
 }
-add_filter( 'get_comments_number', 'blankslate_comment_count', 0 );
-function blankslate_comment_count( $count ) {
+add_filter( 'get_comments_number', 'diegoworks_comment_count', 0 );
+function diegoworks_comment_count( $count ) {
 if ( !is_admin() ) {
 global $id;
 $get_comments = get_comments( 'status=approve&post_id=' . $id );
